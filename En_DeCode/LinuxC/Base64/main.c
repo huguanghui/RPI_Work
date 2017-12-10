dcd509407fc521d56a4e16e3621067bdc446f9ec
hugh <522146829@qq.com>
2017-12-10 23:06:09 +0800

    add base64

--- En_DeCode/LinuxC/Base64/main.c
+++ En_DeCode/LinuxC/Base64/main.c
@@ -6,9 +6,22 @@ int main()
 	unsigned char str[] = "s13";
 	char *s = NULL;
 
-	s = s2base64(str, NULL, sizeof(str)/sizeof(char));
+	char *str1 = malloc(10);
+	char *p = str1;
+	memset(str1, 0, 10);
+
+	*str1 ++ = 'a';
+
+	printf("P:%p\n", p);
+	printf("o:%c\n", *p);
+	printf("str:%p\n", str1);
+	printf("str:%c\n", *str1);
+
+	s = s2base64(str, NULL, sizeof(str)/sizeof(str[0]));
 	printf("S:%s\n", s);
 	free(s);
+
+
 
 	return iRet;
 }
