0c85e54d535fcbf8ef80218af34d0e69510fa891
huguanghui <522146829@qq.com>
2018-02-04 12:38:31 +0800

    refresh

--- Docs/G90舵机/sg90.py
+++ Docs/G90舵机/sg90.py
@@ -16,14 +16,14 @@ time.sleep(2)
 
 while(True):
 	for i in range(0,181,10):
-		printf (i)
+		print (i)
 		p.ChangeDutyCycle(2.5 + 10*i/180)
 		time.sleep(0.02)
 		p.ChangeDutyCycle(0)
 		time.sleep(0.2)
 
 	for i in range(181,0,-10):
-		printf (i)
+		print (i)
 		p.ChangeDutyCycle(2.5 + 10*i/180)
 		time.sleep(0.02)
 		p.ChangeDutyCycle(0)
